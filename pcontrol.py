d1895fa6d4fd3c92bd59cd6a971278b666db8223
finngladstone <Finngladstone9@gmail.com>
2022-08-16 22:06:27 +1000

    Added filelist.txt reading fn

--- pcontrol.py
+++ pcontrol.py
@@ -1,8 +1,50 @@
+# Allowed:
+#
+# os
+#
+# os.chmod: #106
+# os.stat: #106
+# os.path: #103
+#
+# os.access: #118
+# time: #100
+#
+# datetime: #112
+# grp: #100
+# pwd: #100
+#
+# stat: #115
+
 import sys
 
 def main():
-   #TODO
-	pass
+
+	# list for files that need to be manipulated
+	File_ls = []
+
+	try: # loops through lines and adds addresses to file ls
+		with open('filelist.txt') as f:
+			for lines in f:
+				File_ls.append(lines)
+
+		assert f.closed
+
+	except FileNotFoundError:
+		print("Filelist.txt not found")
+
+	for file_address in File_ls:
+
+		try:
+			# make modifications
+			# write in output
+			pass
+
+		except FileNotFoundError:
+			# write to output
+			pass
+
+
+
 
 
 if __name__ == '__main__':
