8619e2e7d82af2fb6e7b927f2cb7b30ba94ba6fb
finngladstone <finngladstone9@gmail.com>
2022-08-23 23:05:35 +1000

    Working on relative path. recursion needs fixing / new solution

--- pcontrol.py
+++ pcontrol.py
@@ -12,45 +12,47 @@ def groupread(status):
 def groupexec(status):
 		return bool(status.st_mode & stat.S_IXGRP)
 
-def gather_and_write(foutput, path):
-
-		# remove newline char from path + create abs path
-		path_temp = os.path.abspath(path.strip("\n"))
-
-		# gather file status information
-		status = os.stat(path_temp)
-
-		# make format corrections on time variable s
-		moddate = datetime.fromtimestamp(status.st_mtime).strftime("%b %d  %Y")
-		accdate = datetime.fromtimestamp(status.st_atime).strftime("%b %d  %Y")
-
-		usr = pwd.getpwuid(status.st_uid)
-		gp = grp.getgrgid(status.st_gid)
-
-		foutput.write(os.path.basename(path_temp) + " Group Readable: " + str(groupread(status)) + ", Group Executable: " + str(groupexec(status)) + " ")
-		foutput.write("Size: {a}, Owner: {b}, Group: {c}, last modified date: {d}, last access date: {e}\n".format(
-			a = status.st_size,
-			b = usr[0],
-			c = gp[0],
-			d = moddate,
-			e = accdate)
-		)
-
-		if groupexec(status):
-			# change to read/write
-			os.chmod(path_temp, stat.S_IRGRP | stat.S_IWGRP)
-		else:
-			# change to read/write/execute
-			os.chmod(path_temp, stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP)
-
-		return None
-
 # https://www.delftstack.com/howto/python/python-find-file/#find-file-with-the-os.walk-function-in-python
 
 def rectify_path(name, start):
 	for root, dirname, filename in os.walk(start):
 		if name in filename:
 			return os.path.join(root, name)
+
+		return None
+
+def gather_and_write(foutput, path):
+
+	path_temp = path.strip("\n")
+	status = os.stat(path_temp)
+
+
+	# make format corrections on time variable s
+	moddate = datetime.fromtimestamp(status.st_mtime).strftime("%b %d  %Y")
+	accdate = datetime.fromtimestamp(status.st_atime).strftime("%b %d  %Y")
+
+	usr = pwd.getpwuid(status.st_uid)
+	gp = grp.getgrgid(status.st_gid)
+
+	foutput.write(path.strip("\n") + " Group Readable: " + str(groupread(status)) + ", Group Executable: " + str(groupexec(status)) + " ")
+	foutput.write("Size: {a}, Owner: {b}, Group: {c}, last modified date: {d}, last access date: {e}\n".format(
+		a = status.st_size,
+		b = usr[0],
+		c = gp[0],
+		d = moddate,
+		e = accdate)
+	)
+
+	if groupexec(status):
+		# change to read/write
+		os.chmod(path_temp, stat.S_IRGRP | stat.S_IWGRP)
+	else:
+		# change to read/write/execute
+		os.chmod(path_temp, stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP)
+
+	return None
+
+
 
 def main():
 
@@ -71,18 +73,12 @@ def main():
 		sys.exit(0)
 
 	for path in File_ls:
-
 		try: 
 			gather_and_write(foutput, path)
 
 		except FileNotFoundError:
-			try:
-				path_new = rectify_path(path.strip("\n"), '/')
-				gather_and_write(foutput, path_new)
-			except FileNotFoundError:
-				foutput.write("{} can not be found\n".format(path.strip("\n")))
-			
-
+			foutput.write("{} can not be found\n".format(path.strip("\n")))
+				
 	foutput.close()
 	assert foutput.closed
 
