fe2a250102b0f5c3adb8a470c49968449e87f870
finngladstone <finngladstone9@gmail.com>
2022-08-25 15:36:12 +1000

    Finished making test cases

--- tests/test.sh
+++ tests/test.sh
@@ -1,59 +1,93 @@
+shopt -s extglob # allows us to reset the directory in one line 
+
 today=`date '+%b %d  %Y'`
 name=`id -u -n`
 group=`id -gn`
 
-# resets directory 
-PRESERVE="test.sh"|"pcontrol.py"
-
-rm -rf !$PRESERVE
+# resets directory - https://superuser.com/questions/529854/how-to-delete-all-files-in-a-directory-except-some
+rm -rf !("test.sh"|"pcontrol.py")
 
 # test program correctly handles missing file list 
 
 touch exp_output.txt 
-echo "" > exp_output.txt
+echo "filelist.txt can not be found" > exp_output.txt
 
+check_1() {
+    if diff exp_output.txt output.txt; then
+        check_1_result='PASSED'
+    else 
+        check_1_result='FAILED'
+    fi
+}
+
+echo "--- First execution cycle ---"
+python3 pcontrol.py # should run, create output.txt, and report missing filelist.txt
+check_1
+echo "Testing program handles missing filelist.txt... $check_1_result" 
+
+# create filelist but add invalid path 
+touch filelist.txt 
+echo "test_1.ext" > filelist.txt 
+echo "test_1.ext can not be found" > exp_output.txt
+
+echo "--- Second execution cycle ---"
 python3 pcontrol.py
+
+check_1
+echo "Testing program handles invalid file name... $check_1_result"
 
 # start by testing permissions flip: examine chmod values as well as output!
 
 touch test_1.ext 
 touch test_2.ext 
 
-echo "initialised test_1 and test_2"
+#initialised test_1 and test_2
 
 chmod 463 test_1.ext 
 chmod 173 test_2.ext 
 
-echo "test_1 set to group read/write @ 463"
-echo "test_2 set to group r/w/exec @ 173"
+# test_1 set to group read/write @ 463
+# test_2 set to group r/w/exec @ 173
 
 test_1_size=`du -k "test_1.ext" | cut -f1`
 test_2_size=`du -k "test_2.ext" | cut -f1`
 
+# create + modify filelist to include the two test files 
 
-
-touch filelist.txt 
-
-echo "test_1.ext
-test_2.ext" > filelist.txt
+echo "test_2.ext" >> filelist.txt
 
 touch exp_output.txt 
 echo "test_1.ext Group Readable: True, Group Executable: False Size: $test_1_size, Owner: $name, Group: $group, last modified date: $today, last access date: $today" > exp_output.txt
 echo "test_2.ext Group Readable: True, Group Executable: True Size: $test_2_size, Owner: $name, Group: $group, last modified date: $today, last access date: $today" >> exp_output.txt
 
+echo "--- Third execution cycle ---"
 python3 pcontrol.py
 
-diff exp_output.txt output.txt # tests if output is correctly formatted! 
+check_1 # run diff test again
+
+echo "Testing output file formatting, correct reading of permissions... $check_1_result"
 
 # https://stackoverflow.com/questions/26949492/how-to-check-chmod-using-bash-numeric
 
-test_1_perms=`stat -c "%a" test_1.ext`
-test_2_perms=`stat -c "%a" test_2.ext`
 
-echo "chmod for test_1: $test_1_perms"
-echo "chmod for test_2: $test_2_perms"
+check_2() { # checks that permissions bits have been correctly flipped for test_1 and test_2
+    test_1_perms=`stat -c "%a" test_1.ext`
+    test_2_perms=`stat -c "%a" test_2.ext`
+    if [[ test_1_perms -eq 473 ]]; then
+        if [[ test_2_perms -eq 163 ]]; then 
+            check_2_result='PASSED'
+        else 
+            check_2_result='FAILED'
+        fi
+    else 
+        check_2_result='FAILED'
+    fi
+}
 
-# test if program recognises files in subdirectories 
+check_2
+
+echo "Testing that permissions bits are correctly modified in chmod... $check_2_result"
+
 mkdir -p moretests
 touch moretests/test_3.ext 
 
@@ -73,10 +107,21 @@ echo "test_1.ext Group Readable: True, Group Executable: True Size: $test_1_size
 echo "test_2.ext Group Readable: True, Group Executable: False Size: $test_2_size, Owner: $name, Group: $group, last modified date: $today, last access date: $today" >> exp_output.txt
 echo "test_3.ext Group Readable: True, Group Executable: False Size: $test_3_size, Owner: $name, Group: $group, last modified date: $today, last access date: $today" >> exp_output.txt
 
+echo "--- Fourth Execution Cycle ---"
 python3 pcontrol.py 
 
-diff exp_output.txt output.txt 
+check_1 # run diff test again
 
-test_3_perms=`stat -c "%a" moretests/test_3.ext`
-echo $test_3_perms  
+echo "Testing for file in subdirectory + permissions flip in output.txt... $check_1_result"
 
+check_3() {
+    test_3_perms=`stat -c "%a" moretests/test_3.ext`
+    if [[ test_3_perms -eq 374 ]]; then
+        check_3_result='PASSED'
+    else 
+        check_3_result='FAILED'
+    fi
+} 
+
+check_3
+echo "Testing that file in subdirectory was successfully flipped in chmod... $check_3_result"
